228e885e585a753cae52d0ea47bd55c8ba5c0e30
Raphael Santiago <raphael.sant.emp@gmail.com>
2023-11-27 23:18:09 -0300

    Atividade Concluída, requisitos cumpridos com bônus de consulta utilizando PokemonAPI

--- lib/page/climate_page.dart
+++ lib/page/climate_page.dart
@@ -1,11 +1,74 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 import 'package:flutter/material.dart';
 
-class ClimatePage extends StatelessWidget {
+class ClimatePage extends StatefulWidget {
+  @override
+  _ClimaNowPage createState() => _ClimaNowPage();
+}
+
+class _ClimaNowPage extends State<ClimatePage> {
+  final TextEditingController _cityController = TextEditingController();
+  String _temperatureMax = '';
+  String _temperatureMin = '';
+  String _weatherState = '';
+
+  Future<void> fetchWeather(String city) async {
+    final apiKey = 'd8e593330e044e1e8af02146232811'; // Insira sua chave da WeatherAPI aqui
+    final apiUrl = 'https://api.weatherapi.com/v1/forecast.json?key=$apiKey&q=$city&days=1&lang=pt';
+    print(city);
+
+    try {
+      final response = await http.get(Uri.parse(apiUrl));
+      if (response.statusCode == 200) {
+        final jsonData = jsonDecode(response.body);
+        setState(() {
+          _temperatureMax = jsonData['forecast']['forecastday'][0]['day']['maxtemp_c'].toString();
+          _temperatureMin = jsonData['forecast']['forecastday'][0]['day']['mintemp_c'].toString();
+          _weatherState = jsonData['forecast']['forecastday'][0]['day']['condition']['text'];
+        });
+      } else {
+        print('ERROOOOOOOOOOOOOOOOOOOOOOOOOO 0001');
+        throw Exception('Failed to load data');
+      }
+    } catch (e) {
+      print('ERROOOOOOOOOOOOOOOOOOOOOOOOOO 0002');
+      print('Error: $e');
+    }
+  }
   @override
   Widget build(BuildContext context) => Scaffold(
     appBar: AppBar(
-      title: Text('Clima'),
+      title: Text('Clima Agora!'),
+      backgroundColor: Colors.blue,
     ),
-    body:  Center(child: Text('Climatização Aqui', style: TextStyle(fontSize: 60),),),
+    body: Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text('Insira a cidade desejada e saiba as temperaturas mínima e máxima atualizadas juntamente com o estado meteorológico!'),
+          TextField(
+            controller: _cityController,
+            decoration: InputDecoration(
+              labelText: 'Insira o nome da cidade',
+            ),
+          ),
+          SizedBox(height: 20.0),
+          ElevatedButton(
+            onPressed: () {
+              fetchWeather(_cityController.text);
+            },
+            child: Text('Consultar Clima!'),
+          ),
+          SizedBox(height: 20.0),
+          Text('Temperatura Max: $_temperatureMax°C'),
+          Text('Temperature Min : $_temperatureMin°C'),
+          Text('Estado Meteorológico: $_weatherState'),
+        ],
+      ),
+    ),
   );
 }
--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,23 +8,70 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int count = 0;
+  final TextEditingController _pokemonController = TextEditingController();
+  String _pokemonName = '';
+  String _pokemonImage = '';
 
-  void increment() {
-    setState(() {
-      count++;
-    });
+  Future<void> fetchPokemon(String nameOrId) async {
+    final apiUrl = 'https://pokeapi.co/api/v2/pokemon/$nameOrId';
+
+    try {
+      final response = await http.get(Uri.parse(apiUrl));
+      if (response.statusCode == 200) {
+        final jsonData = jsonDecode(response.body);
+        setState(() {
+          _pokemonName = jsonData['name'];
+          _pokemonImage = jsonData['sprites']['front_default'];
+        });
+      } else {
+        throw Exception('Failed to load data');
+      }
+    } catch (e) {
+      print('Error: $e');
+    }
   }
 
   @override
   Widget build(BuildContext context) => Scaffold(
     appBar: AppBar(
-      backgroundColor: Colors.blue,
-      title: Text('Página Inicial'),
+      title: Text('PokeDex'),
     ),
-    body:  Center(child: Text('Home ' + count.toString(), style: TextStyle(fontSize: 60),),),
-    floatingActionButton: FloatingActionButton(
-      onPressed: increment,
+    body: Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          TextField(
+            controller: _pokemonController,
+            decoration: InputDecoration(
+              labelText: 'Enter Pokemon name or ID',
+            ),
+          ),
+          SizedBox(height: 20.0),
+          ElevatedButton(
+            onPressed: () {
+              fetchPokemon(_pokemonController.text.toLowerCase());
+            },
+            child: Text('Search'),
+          ),
+          SizedBox(height: 20.0),
+          _pokemonName.isNotEmpty
+              ? Column(
+            children: [
+              Text(
+                'Name: $_pokemonName',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 10.0),
+              _pokemonImage.isNotEmpty
+                  ? Image.network(_pokemonImage)
+                  : SizedBox(),
+            ],
+          )
+              : SizedBox(),
+        ],
+      ),
     ),
   );
 }
